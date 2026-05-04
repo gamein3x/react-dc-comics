ea312bbe91becc7d571aaa49def8a66ce175a94f
Ivan Caldarella <gamein3x@gmail.com>
2026-05-05 01:32:57 +0200

    Aggiunto stile a Navbar e Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import Navbar from "./Navbar";
 
 export default function Header() {
     return (
-        <>
+        <div className="bg-body-tertiary">
             <Navbar />
-        </>
+        </div>
     );
 }
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,67 +1,81 @@
-import styles from "./Navbar.module.css" 
+import styles from "./Navbar.module.css"
 import logo from '../../assets/img/dc-logo.png';
 import navData from './navData';
 
+
+
 const navLogo = (
-  <a className="navbar-brand" href="#">
-    <img src={logo} alt="DC Logo" style={{ height: 40 }} />
-  </a>
+    <div className="head-item0">
+        <a className="navbar-brand" href="#">
+            <img src={logo} alt="DC Logo" style={{ height: 40 }} />
+        </a>
+    </div>
 );
 
 const navLink = (title, idx) => (
-  <a
-    className="nav-link dropdown-toggle"
-    href="#"
-    id={`navbarDropdown-${idx}`}
-    role="button"
-    data-bs-toggle="dropdown"
-    aria-expanded="false"
-  >
-    {title}
-  </a>
+    <a
+        className="nav-link dropdown-toggle"
+        href="#"
+        id={`navbarDropdown-${idx}`}
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+    >
+        {title}
+    </a>
 );
 
 const navDropList = (item, idx) => (
-  <ul className="dropdown-menu" aria-labelledby={`navbarDropdown-${idx}`}>
-    {item.itemList.map((li, j) => (
-      <li key={j}>
-        <a className="dropdown-item" href="#">
-          {li}
-        </a>
-      </li>
-    ))}
-  </ul>
+    <ul className="dropdown-menu" aria-labelledby={`navbarDropdown-${idx}`}>
+        {item.itemList.map((li, key) => (
+            <li key={key}>
+                <a className="dropdown-item" href="#">
+                    {li}
+                </a>
+            </li>
+        ))}
+    </ul>
 );
 
 const navListItem = navData.map((item, idx) => (
-  <li key={idx} className="nav-item dropdown">
-    {navLink(item.itemTitle, idx)}
-    {navDropList(item, idx)}
-  </li>
+    <li key={idx} className="nav-item dropdown">
+        {navLink(item.itemTitle, idx)}
+        {navDropList(item, idx)}
+    </li>
 ));
 
+
+
 const navbar = (
-  <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      {navLogo}
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav ms-auto">{navListItem}</ul>
-      </div>
+    <div className="head-item1">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav ms-auto">{navListItem}</ul>
+                </div>
+            </div>
+        </nav>
     </div>
-  </nav>
+);
+
+const headContentBox = (
+    <div className="d-flex justify-content-around my-1 h2">
+        {navLogo}
+        {navbar}
+    </div>
 );
 
 export default function Navbar() {
-  return navbar;
+    return headContentBox;
 }
